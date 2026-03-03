9c83303f9d1aae3528d62e352826d7e9d30da0a0
Anjali Pandey <anjalipandey52005@gmail.com>
2026-03-04 01:57:37 +0530

    rec

---
+++ recursion/friendsPairing.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class friendsPairing {
+    public static int ways(int n){
+        if (n==0 || n==1) {
+            return 1;
+        }
+        return ways(n-1) + (n-1)*ways(n-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(ways(3));
+    }
+}
